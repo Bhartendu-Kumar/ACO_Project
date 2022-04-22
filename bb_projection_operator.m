5558e916784e197509ad9ae5f72e9ebcbaf3d603
Bhartendu-Kumar <43710040+Bhartendu-Kumar@users.noreply.github.com>
2022-04-23 04:11:46 +0530

    Delete bb_projection_operator.m

--- bb_projection_operator.m
+++
@@ -1,4 +0,0 @@
-function return_value = bb_projection_operator(input_variable,parameter_l)
-    temp = input_variable;
-return_value = min(max(input_variable,-ones(size(input_variable))*parameter_l),ones(size(input_variable))*parameter_l);
-end
