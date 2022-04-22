cefb84a14c3f1c2fb3ffc9042820a9461e200c83
Bhartendu-Kumar <43710040+Bhartendu-Kumar@users.noreply.github.com>
2022-04-23 04:17:54 +0530

    Delete operator_l2_projection.m

--- operator_l2_projection.m
+++
@@ -1,5 +0,0 @@
-function projected_input_vector = proj_u(vector_to_project, parameter_l)
-
-projected_input_vector = (-parameter_l<=vector_to_project & vector_to_project<=parameter_l).*vector_to_project + (vector_to_project>parameter_l).*parameter_l + (vector_to_project<-parameter_l).*-parameter_l;
-
-end
