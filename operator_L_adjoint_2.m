4b482b24f7295463e04f298b93c7d2ba74f2066d
Bhartendu-Kumar <43710040+Bhartendu-Kumar@users.noreply.github.com>
2022-04-23 04:17:09 +0530

    Delete operator_L_adjoint_2.m

--- operator_L_adjoint_2.m
+++
@@ -1,9 +0,0 @@
-function [ matrix_pair_p, matrix_pair_q ] = operator_L_adjoint_2( input_image )
-
-
-[rows, colns] = size(input_image);
-
-matrix_pair_p = input_image(1:rows-1, :) - input_image(2:rows, :);
-matrix_pair_q = input_image(:,1:colns-1) - input_image(:, 2:colns);
-end
-
